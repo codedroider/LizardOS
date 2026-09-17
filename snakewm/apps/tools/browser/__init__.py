7463cf6bb64f1f8b5b8d054b4c1463166df69a16
𓆩 𝐤𝐨𝐝𝐞𝐫𝐨𝐯 𓆪 <kdrv.gh@gmail.com>
2026-09-17 18:02:13 +0300

    Update __init__.py

--- snakewm/apps/tools/browser/__init__.py
+++ snakewm/apps/tools/browser/__init__.py
@@ -1,13 +1,14 @@
-import threading
 import os
+import json
+import threading
+import pygame
+from pygame_gui.elements import UIProgressBar
 
 try:
     import webview
 except ImportError:
     os.system('pip install pywebview')
     import webview
-
-from pygame_gui.elements import UIProgressBar
 
 CUSTOM_USER_AGENT = (
     "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 "
@@ -16,9 +17,49 @@ CUSTOM_USER_AGENT = (
     "LizardBrowser/1.0 (Privacy-First; Don't-Track-Me; No-Fingerprint)"
 )
 
-HOME_URL = 'https://codedroider.github.io/codesearch/'
+HOME_URL = 'https://github.io'
 
-def launch_browser(start_url, manager, progress_bar):
+class BrowserAPI:
+    def __init__(self):
+        self.window = None
+
+    def inject_file_data(self, b64_data, filename):
+        js_injector = rf'''
+        (function() {{
+            var fileInput = document.querySelector('input[type="file"]');
+            if (fileInput) {{
+                var byteCharacters = atob("{b64_data}");
+                var byteNumbers = new Array(byteCharacters.length);
+                for (var i = 0; i < byteCharacters.length; i++) {{
+                    byteNumbers[i] = byteCharacters.charCodeAt(i);
+                }}
+                var byteArray = new Uint8Array(byteNumbers);
+                var blob = new Blob([byteArray], {{type: "application/octet-stream"}});
+                
+                var dataTransfer = new DataTransfer();
+                var file = new File([blob], {json.dumps(filename)}, {{type: "application/octet-stream"}});
+                dataTransfer.items.add(file);
+                fileInput.files = dataTransfer.files;
+                
+                fileInput.dispatchEvent(new Event('change', {{ bubbles: true }}));
+            }}
+        }})();
+        '''
+        if self.window:
+            try:
+                self.window.evaluate_js(js_injector)
+            except Exception:
+                pass
+
+    def load_fallback_url(self, file_path):
+        if self.window:
+            file_url = f"file://{os.path.abspath(file_path)}".replace("\\", "/")
+            try:
+                self.window.load_url(file_url)
+            except Exception:
+                pass
+
+def launch_browser(start_url, manager, progress_bar, api_instance):
     webview.settings = {
         'ALLOW_DOWNLOADS': True,
         'ALLOW_FILE_URLS': True
@@ -33,8 +74,11 @@ def launch_browser(start_url, manager, progress_bar):
         url=start_url,
         width=1024,
         height=720,
-        resizable=True
+        resizable=True,
+        js_api=api_instance
     )
+    
+    api_instance.window = window
     
     def simulate_progress():
         import time
@@ -59,9 +103,18 @@ def load(manager, params=None):
     )
     progress_bar.set_progress(0)
 
+    api_instance = BrowserAPI()
+
+    from dnd_fm import DnDFileManager
+    file_manager = DnDFileManager(
+        pygame.Rect((50, 50), (400, 500)),
+        manager,
+        api_instance=api_instance
+    )
+
     browser_thread = threading.Thread(
         target=launch_browser, 
-        args=(start_url, manager, progress_bar), 
+        args=(start_url, manager, progress_bar, api_instance), 
         daemon=True
     )
     browser_thread.start()
